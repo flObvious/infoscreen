dbdd9e37feb9058a0e4ca68bdfc46c5855e3b7bd
Staub Floris (stbf) <stbf@zhaw.ch>
2017-02-02 13:16:55 +0100

    added js function for clock and css adjustments

--- js/page.js
+++ js/page.js
@@ -23,9 +23,9 @@ function clock() {
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
-  }
+}
 
 function datum() {
   var d = new Date();
